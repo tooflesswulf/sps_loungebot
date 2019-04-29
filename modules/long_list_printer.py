eee32eb586c7ec76f11651ce1a71bc8bb1742efc
albertxu <17albertX@students.harker.org>
2019-04-28 22:24:23 -0400

    nitro list will now timeout and self-annihilate after a while.

--- modules/long_list_printer.py
+++ modules/long_list_printer.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
 import discord
 
+import time
+import asyncio
+
 
 class EmbedListPrinter(commands.Cog):
+    list_timeout = 6000  # 10 min
+
     def __init__(self, ctx, max_len=20):
         self.bot = ctx.bot
         self.author = ctx.author
@@ -12,6 +17,7 @@ class EmbedListPrinter(commands.Cog):
         self.page_len = 0
         self.pages = []
         self.page_no = 0
+        self.last_time = 0
 
         self.title = None
         self.msg = None
@@ -46,8 +52,9 @@ class EmbedListPrinter(commands.Cog):
         e = discord.Embed(title=self.title)
         for name, value, inline in self.fields[self.pages[self.page_no]]:
             e.add_field(name=name, value=value, inline=inline)
-        e.set_footer(text='Page {}/{}'.format(1+self.page_no, len(self.pages)))
+        e.set_footer(text='Page {}/{}'.format(1 + self.page_no, len(self.pages)))
         self.msg = await ctx.send(embed=e)
+        self.last_time = time.time()
 
         if len(self.pages) != 1:
             discord.utils.get(ctx.bot.emojis, name='⬅')
@@ -55,6 +62,19 @@ class EmbedListPrinter(commands.Cog):
             await self.msg.add_reaction('➡')
 
             self.bot.add_listener(self.change_page_listener, name='on_reaction_add')
+
+            # this is a bit jank; we create a future object here
+            t = asyncio.ensure_future(self.wait_timeout())
+            # usually we'd `await t` to wait for timeout to finish, but we don't actually care lmao
+
+    async def wait_timeout(self):
+        while time.time() - self.last_time < self.list_timeout:
+            await asyncio.sleep(self.list_timeout / 10)
+
+        self.bot.remove_listener(self.change_page_listener, name='on_reaction_add')
+        e = discord.Embed(title=self.title, description='This list has timed out.')
+        await self.msg.edit(embed=e)
+        await self.msg.clear_reactions()
 
     async def change_page_listener(self, rxn, user):
         if rxn.message.id != self.msg.id or len(self.pages) == 1 or user != self.author:
@@ -67,7 +87,7 @@ class EmbedListPrinter(commands.Cog):
             self.page_no -= 1
         elif rxn.emoji == '➡':
             await rxn.remove(user)
-            if self.page_no+1 == len(self.pages):
+            if self.page_no + 1 == len(self.pages):
                 return
 
             self.page_no += 1
@@ -77,5 +97,7 @@ class EmbedListPrinter(commands.Cog):
         e = discord.Embed(title=self.title)
         for name, value, inline in self.fields[self.pages[self.page_no]]:
             e.add_field(name=name, value=value, inline=inline)
-        e.set_footer(text='Page {}/{}'.format(1+self.page_no, len(self.pages)))
+        e.set_footer(text='Page {}/{}'.format(1 + self.page_no, len(self.pages)))
         await self.msg.edit(embed=e)
+
+        self.last_time = time.time()
